8ccd930dbe91446cd2fd932e970503976cb5916e
csecoders <csecodingclub@gmail.com>
2014-09-02 17:52:53 +0530

    Solution to problem 1
    The program’s logic as explained in the club meeting is as follows:
    
    1) Find the number of occurrences of each character in the word.
    
    2) If there are more than two characters with an odd number of occurrences, a palindrome cannot be formed by shuffling the elements.
    
    3) If there are no elements with an odd number of occurrence or if there is only one element with an odd number of occurrence, then a palindrome can be formed.

---
+++ problem1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main()
+{
+    //Gets the input string
+    char inputString[25];
+    //Array to hold the occurances
+    int alphabets[26] = {0};
+    int i, flag = 0;
+    //Get the input from the user
+    gets(inputString);
+    //Iterate over each character in the input string and increase
+    //its corresponding occurance array element by 1.
+    for (i = 0; inputString[i] != ”; i++)
+    {
+        alphabets[(int)(inputString[i] – ‘a’)]++;
+    }
+    //Check for characters with odd no. of occurances
+    for(i = 0; i < 26; i++)
+    {
+        if((alphabets[i] % 2) != 0)
+        {
+            flag++;
+        }
+    }
+    //Dislay possible if less than 1 character with
+    // odd number of occurances.
+    if(flag > 1)
+    {
+        printf(“Not possible”);
+    }
+    else
+    {
+        printf(“Possible”);
+    }
+    return 0;
+   }
