d1f2a5c63bd06cb023a8fe6b63703c669face005
csecoders <csecodingclub@gmail.com>
2014-09-09 20:39:28 +0530

    Create numberToWords.cpp

---
+++ numberToWords.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int n;
+    char ar[10][15] = {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+   cin>> n;
+   cout<<ar[n];
+   
+   
+   return 0;
+}
+
